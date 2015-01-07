9d41f3dc13c86dd3adf4157197456381afc43c45
Marc Palluat de Besset <marc.palluatdebesset@hibu.com>
2015-01-07 14:28:42 +0000

    Initial Commit

---
+++ NETKit/NETKit.h
@@ -0,0 +1,13 @@
+//
+//  NETKit.h
+//  NETKit
+//
+//  Created by Marc Palluat de Besset on 07/01/2015.
+//  Copyright (c) 2015 hibu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NETKit : NSObject
+
+@end
---
+++ NETKit/NETKit.m
@@ -0,0 +1,13 @@
+//
+//  NETKit.m
+//  NETKit
+//
+//  Created by Marc Palluat de Besset on 07/01/2015.
+//  Copyright (c) 2015 hibu. All rights reserved.
+//
+
+#import "NETKit.h"
+
+@implementation NETKit
+
+@end
